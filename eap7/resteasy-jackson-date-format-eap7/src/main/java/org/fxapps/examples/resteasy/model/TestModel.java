ce17999e4495c6ba313084b0cf99d76a329ca226
William Antonio <william.fatecsjc@gmail.com>
2016-08-01 13:41:07 -0300

    Adding EAP 7 example

---
+++ eap7/resteasy-jackson-date-format-eap7/src/main/java/org/fxapps/examples/resteasy/model/TestModel.java
@@ -0,0 +1,17 @@
+package org.fxapps.examples.resteasy.model;
+
+import java.util.Date;
+
+public class TestModel {
+	
+	private Date date;
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+}
